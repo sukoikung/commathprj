4f9fbea0a3cc112511556af16ae9fb356e1fb225
Kontorn Kaewaram <kontorn.ka.61@ubu.ac.th>
2020-09-30 09:19:35 +0700

    Updates

---
+++ precache-manifest.16161feb67a81d960dfaf3e410470a96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08adcd3da034a68bd8dee00595928722",
+    "url": "/commathprj/index.html"
+  },
+  {
+    "revision": "f56181974edcb9f8cac9",
+    "url": "/commathprj/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e558723c49384c718342",
+    "url": "/commathprj/static/js/2.8dff6ca4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commathprj/static/js/2.8dff6ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56181974edcb9f8cac9",
+    "url": "/commathprj/static/js/main.97e36860.chunk.js"
+  },
+  {
+    "revision": "d1a43afb4928bdeb16d6",
+    "url": "/commathprj/static/js/runtime-main.41a99890.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commathprj/static/media/logo.5d5d9eef.svg"
+  }
+]);
