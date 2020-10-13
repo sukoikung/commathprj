9efe85c2580343b02ffc68f7526529f5ebd4de28
Kontorn Kaewaram <kontorn.ka.61@ubu.ac.th>
2020-10-13 16:09:50 +0700

    Updates

---
+++ precache-manifest.7e0ea9edacbe0bb508a3530ce72517f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f6b195247ba0249e7018015ee893fa",
+    "url": "/commathprj/index.html"
+  },
+  {
+    "revision": "ba9d0769c9ad240f5954",
+    "url": "/commathprj/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "094beac6e4eaa5ee7bcf",
+    "url": "/commathprj/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba9d0769c9ad240f5954",
+    "url": "/commathprj/static/js/2.12fee346.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/commathprj/static/js/2.12fee346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094beac6e4eaa5ee7bcf",
+    "url": "/commathprj/static/js/main.786b0e3c.chunk.js"
+  },
+  {
+    "revision": "d1a43afb4928bdeb16d6",
+    "url": "/commathprj/static/js/runtime-main.41a99890.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commathprj/static/media/logo.5d5d9eef.svg"
+  }
+]);
